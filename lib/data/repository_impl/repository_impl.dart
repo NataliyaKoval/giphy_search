8f95bdcc68603dd522a205e94c10b7c42d6ae1ca
NataliiaKoval <hakunata@gmail.com>
2023-10-05 09:07:00 +0200

    Fix state emitting

--- lib/data/repository_impl/repository_impl.dart
+++ lib/data/repository_impl/repository_impl.dart
@@ -2,7 +2,8 @@ import 'package:giphy_search/data/data_sources/rest_api_client.dart';
 import 'package:giphy_search/domain/models/gif_response.dart';
 import 'package:giphy_search/domain/repository/repository.dart';
 
-const apiKey = String.fromEnvironment('API_KEY');
+//const apiKey = String.fromEnvironment('API_KEY');
+const apiKey = 'Ed4XUGmUmrWbYu1za4vIRrPQwGVSWm22';
 const limit = 10;
 
 class RepositoryImpl implements Repository {
